01d3590bab401dd6ca5fcc8ac27043f95fb89423
Miro <mr.mladenoff@gmail.com>
2018-12-04 10:14:52 -0800

    Pics search app Finished

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class ImageCard extends Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
 
@@ -10,12 +11,16 @@ class ImageCard extends Component {
     this.imageRef.current.addEventListener("load", this.setSpans);
   }
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
   };
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
